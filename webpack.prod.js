811a87fcc3e052d51712e7339a22cd66779dcde2
AndhikaArdi <127767610+AndhikaArdi@users.noreply.github.com>
2025-06-06 12:59:58 +0700

    minor fixxing
    memperabaiki publicpath

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,7 +10,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = merge(common, {
   mode: 'production',
   output: {
-    publicPath: '/StoryApp-dicoding-pwa/',
+    publicPath: '/SkinSyncApp/',
   },
   module: {
     rules: [
